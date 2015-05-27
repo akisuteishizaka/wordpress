43ca1945c817adbc8108b59200e6a2c293b2591c
taiti <taiti@stone-rise.com>
2015-05-27 21:11:43 +0900

    development - 19

--- wp-content/themes/wordpress/footer.php
+++ wp-content/themes/wordpress/footer.php
@@ -1,5 +1,5 @@
 	<div id="footer" class="container">
-		Copyright 2015 <?php if (date("Y")!=2015) echo date("-Y"); ?>
+		Copyright 2015 <?php if (date("Y")!=2015) echo date("-Y"); ?> All rights reserved, stone-rise.com
 	</div>
 	<?php wp_footer(); ?>
 </body>
