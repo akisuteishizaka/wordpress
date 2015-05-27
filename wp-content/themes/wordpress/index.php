8beab89a77636f22aecc543e1ce644104658b53b
taiti <taiti@stone-rise.com>
2015-05-27 18:35:34 +0900

    development_2015052701

---
+++ wp-content/themes/wordpress/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>初めてのWordPress</title>
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="header" class="container">
+		<h1><a href="">はじめてのWordPress</a></h1>
+		<ul class="menu">
+			<li><a href="">menu</a></li>
+			<li><a href="">menu</a></li>
+			<li><a href="">menu</a></li>
+		</ul>
+	</div>
+	<div id="main" class="container">
+		<div id="posts" >
+			<div class="post">
+				<div class="post-header">
+					<h2><a href="">タイトル</a></h2>
+					<div class="post-content">
+						<div class="post-image">
+							<img src="images.jpg" height="168" width="300">
+						</div>
+						<div class="post-body">
+						本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。
+						本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。本文。
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="navigation">
+				<div class="prev">perv</div>
+				<div class="next">next</div>
+			</div>
+		</div>
+		<div id="sidebar" >
+			<div class="widget">
+				<h3>カテゴリー</h3>
+				<ul>
+					<li><a href="">item</a></li>
+					<li><a href="">item</a></li>
+					<li><a href="">item</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div id="footer" class="container">footer</div>
+
+</body>
+</html>
