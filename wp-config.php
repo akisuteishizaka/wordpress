07ad5dcb89e7de6d8195109acd0eeb24755274e8
taiti <taiti@stone-rise.com>
2015-05-27 19:19:25 +0900

    development - 7

--- wp-config.php
+++ wp-config.php
@@ -73,6 +73,8 @@ $table_prefix  = 'wp_';
  * テーマおよびプラグインの開発者には、その開発環境においてこの WP_DEBUG を使用することを強く推奨します。
  */
 define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', true);
+define('WP_DEBUG_LOG', true);
 
 /*エラーメッセージを表示させる場合*/
 ini_set('display_errors', 'On');
