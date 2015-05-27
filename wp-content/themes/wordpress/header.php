5ba4bea928daad0a86e4d7592d25c18e093d2002
taiti <taiti@stone-rise.com>
2015-05-27 18:50:11 +0900

    development - 2

--- wp-content/themes/wordpress/header.php
+++ wp-content/themes/wordpress/header.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>初めてのWordPress</title>
+	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 <body>
 	<div id="header" class="container">
-		<h1><a href="">はじめてのWordPress</a></h1>
+		<h1><a href="<?php echo home_uri('/'); ?>"><?php bloginfo('name'); ?></a></h1>
 		<ul class="menu">
 			<li><a href="">menu</a></li>
 			<li><a href="">menu</a></li>
