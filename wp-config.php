b621f5856bff49c6371d6a59be7d35c67170c0ef
taiti <taiti@stone-rise.com>
2015-12-13 20:15:56 +0900

    wordpress commit #28

--- wp-config.php
+++ wp-config.php
@@ -2,13 +2,18 @@
 /**
  * WordPress の基本設定
  *
- * このファイルは、MySQL、テーブル接頭辞、秘密鍵、ABSPATH の設定を含みます。
- * より詳しい情報は {@link http://wpdocs.sourceforge.jp/wp-config.php_%E3%81%AE%E7%B7%A8%E9%9B%86 
- * wp-config.php の編集} を参照してください。MySQL の設定情報はホスティング先より入手できます。
+ * このファイルは、インストール時に wp-config.php 作成ウィザードが利用します。
+ * ウィザードを介さずにこのファイルを "wp-config.php" という名前でコピーして
+ * 直接編集して値を入力してもかまいません。
  *
- * このファイルはインストール時に wp-config.php 作成ウィザードが利用します。
- * ウィザードを介さず、このファイルを "wp-config.php" という名前でコピーして直接編集し値を
- * 入力してもかまいません。
+ * このファイルは、以下の設定を含みます。
+ *
+ * * MySQL 設定
+ * * 秘密鍵
+ * * データベーステーブル接頭辞
+ * * ABSPATH
+ *
+ * @link http://wpdocs.sourceforge.jp/wp-config.php_%E3%81%AE%E7%B7%A8%E9%9B%86
  *
  * @package WordPress
  */
@@ -24,7 +29,7 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL データベースのユーザー名 */
-define('DB_USER', 'dbuser');
+define('DB_USER', 'wordpress_user');
 
 /** MySQL データベースのパスワード */
 define('DB_PASSWORD', '123456');
@@ -47,15 +52,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '!%x8UfL/_S!&O8Tb`+5IF@0-4v`kCD(c~h/8|lWtf|vKwX8G6*$ `P!k#eoN[^t*');
-define('SECURE_AUTH_KEY',  'HIgS]S@Tg8DX-#bgu(+-2! [FZLM`1oeurnn)g,ZdJI$bVwgW2)z2d;O2>$t xaG');
-define('LOGGED_IN_KEY',    'ZdCQ:hS$:3yynZB7>|!eqP B1O-Hg|GF[21HG[0LbmZ<t3qng6C KN1a#M*wP/GB');
-define('NONCE_KEY',        'tluDPNveUX]o&~1&c{9IflcA.K-hov-#}b@YGYW%{)FP=a=s|63),chDHe_Cg083');
-define('AUTH_SALT',        '~GWg@8v^-/7=@:p]~R<NLM@A;^firh %<KF7#@30C0}^lTxA:mct51tj.m>J4i39');
-define('SECURE_AUTH_SALT', '|htaAB83I#d$2%h*,ydA{m1T,{DO krG<PI2KC<W?=EnPnmSzw*h|IU*ej&O-L0p');
-define('LOGGED_IN_SALT',   '_]UH-M(W*lHTXS6)ev#rC[O]@Zq6MhvJ`2BEvO[]SE</oun-<A+Bx_hy`d1(bOh#');
-define('NONCE_SALT',       '#s)0%I,{]7^^*M:RR:ksf8l>gJcZgD<*9C^DPPcZ&DgKn.yQd|+>m|nWeBv~E,Zy');
-
+define('AUTH_KEY',         '2SrLW4l4>expZxOy!aFM[xaps|T1$Ds&4?}*DZn?)W-U$#Bk=kI9N9LFrhTY#$[,');
+define('SECURE_AUTH_KEY',  ' O:%8jwy#tCxAxtu;|.1vFHr`@mwpuW=VYgWN/D#*8-&(9|dptMf5t,tg$wt(E/(');
+define('LOGGED_IN_KEY',    '~,hV{FsHF%fJCO;/?M?73zknhZdZ8(@Wcr1TM-_xn{KxIy(Fjt=(_n8vB<%+:/bJ');
+define('NONCE_KEY',        'ML;zmHT|Tm(7bh[>N)oe]9Azf|-5sQ3a4(%ZrUoXbB%OMt:KJN2rlNF;7e m@~5D');
+define('AUTH_SALT',        '+f1C55&sfl|e6q1A q@UX+`:oQ|V|-9&QaUN1CP|p_b+v+{[0KEn&*F,~@^Na^iX');
+define('SECURE_AUTH_SALT', 'l(cxB5-oK%Yp=ICxD=YHgzCq+23exL`_:)eR} #];~Q}#E/[r&5nj_U&J~b}S4KU');
+define('LOGGED_IN_SALT',   ' fo:mv:Z>*S-#i~FKZw(QB~RU+CaE5WHI{-/6+`(}qf2mA( sfm Kd!&td6A|,bb');
+define('NONCE_SALT',       'ye?<84MoTI/D|Bj$W=-!tq8g}-v~:$|fr%(f)qp${BIO^:26]lvEzu8!EBEN-~yH');
 /**#@-*/
 
 /**
@@ -71,13 +75,12 @@ $table_prefix  = 'wp_';
  *
  * この値を true にすると、開発中に注意 (notice) を表示します。
  * テーマおよびプラグインの開発者には、その開発環境においてこの WP_DEBUG を使用することを強く推奨します。
+ *
+ * その他のデバッグに利用できる定数については Codex をご覧ください。
+ *
+ * @link http://wpdocs.osdn.jp/WordPress%E3%81%A7%E3%81%AE%E3%83%87%E3%83%90%E3%83%83%E3%82%B0
  */
-define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', true);
-define('WP_DEBUG_LOG', true);
-
-/*エラーメッセージを表示させる場合*/
-ini_set('display_errors', 'On');
+define('WP_DEBUG', false);
 
 /* 編集が必要なのはここまでです ! WordPress でブログをお楽しみください。 */
 
@@ -87,6 +90,3 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
-/** 設定の追加 **/
-define('FS_METHOD', 'direct');
